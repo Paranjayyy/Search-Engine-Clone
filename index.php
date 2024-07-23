eac5197f3d4ed48c00779ebf98b5dea7125b097a
Paranjayyy <goel.paranjay@gmail.com>
2024-07-23 10:28:23 +0530

    2nd Update

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Doodle</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+
+	<div class="wrapper indexPage">
+
+		<div class="mainSection">
+
+			<div class="logoContainer">
+				<img src="assets/images/Logo.png">
+			</div>
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+					<input class="searchBox" type="text" name="keywords">
+					<input class="searchButton" type="submit" value="Search">
+				</form>
+
+			</div>
+
+
+		</div>
+
+	</div>
+
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Doodle</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+
+	<div class="wrapper">
+
+        <div class="header">
+
+            <div class="headerContent">
+
+                <div class="logoContainer">
+                    <a href="index.php">
+                    <img src="assets/images/Logo.png">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+
+                    <form action="search.php" method="GET">
+
+                        <div class="searchBarContainer">
+
+                            <input class="searchBox" type="text" name="keywords">
+                            <button class="searchButton">
+                                <img src="assets/images/icons/search.png">
+                            </button>
+
+                        </div>
+
+
+                    </form>
+
+
+
+                </div>
+                
+
+            </div>
+
+
+
+        </div>
+		
+
+	</div>
+
+</body>
+</html>
